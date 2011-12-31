fb226aaa8dc77ae0ccb3bebb8564e0231c15f5c9
Haoyu Tan <Haoyu Tan>
2011-12-31 09:20:38 +0800

    A fairly complete configure.ac (without automake).

--- Autotools/jupiter-chapter3/src/main.c
+++ Autotools/jupiter-chapter3/src/main.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#if HAVE_PTHREAD_H
+# include <pthread.h>
+#endif
+
+static void *
+print_it(void * data)
+{
+	printf("Hello from %s!\n", (char *)data);
+	return 0;
+}
+	
 int
 main(int argc, char *argv[])
 {
-	printf("Hello from %s!\n", argv[0]);
+#if HAVE_LIBPTHREAD
+	pthread_t tid;
+	pthread_create(&tid, 0, print_it, argv[0]);	
+	pthread_join(tid, 0);
+#else
+	print_it(argv[0]);
+#endif
 	return 0;
 }
